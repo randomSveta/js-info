98a2c94d9eedc70510fc0ae1acd42714aecade40
Sveta K <test@test.test>
2020-01-06 17:18:42 +0000

    working with variables tasks

--- external-alert.js
+++ external-alert.js
@@ -1,1 +1,2 @@
+'use strict';
 alert('I’m JavaScript!');
---
+++ working-with-variables.js
@@ -0,0 +1,26 @@
+'use strict';
+//https://javascript.info/variables#working-with-variables
+
+/*
+let admin;
+let name;
+
+name = 'John';
+admin = name;
+
+alert(admin);
+*/
+
+// https://javascript.info/variables#giving-the-right-name 
+
+/*
+let ourPlanetName = 'Earth';
+let currentUserName = 'Steve';
+*/
+
+// https://javascript.info/variables#uppercase-const
+
+/*
+const BIRTHDAY = '18.04.1982';
+const age = someCode(BIRTHDAY);
+*/
