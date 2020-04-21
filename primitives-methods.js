5473df9965c166dbf3596d9d4324ffbecbbaea60
Sveta K <test@test.test>
2020-04-21 17:21:50 +0100

    Add Primitives methods task. Refactoring.

---
+++ primitives-methods.js
@@ -0,0 +1,8 @@
+/* //https://javascript.info/primitives-methods#can-i-add-a-string-property
+
+let str = "Hello";
+
+str.test = 5;
+
+alert(str.test);
+//answer undefined(not strict mode) or error(in strict mode), primitives are not objects, they loose "object wrapper" */
